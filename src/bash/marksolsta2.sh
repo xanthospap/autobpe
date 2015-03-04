e9f0f61abb122ec6471a3bfe8e9c5e86058e94d6
bpe2 <bpe@vincenty.ntua>
2015-03-04 14:06:47 +0200

    marksolsta2 works; nice one

--- src/bash/marksolsta2.sh
+++ src/bash/marksolsta2.sh
@@ -60,6 +60,8 @@ function help {
   echo "            This file can be compressed"
   echo "           -x --xs-diffs= specify a file containing the processed stations"
   echo "            and the corresponding coordinate differences. (Note 1)"
+  echo "           -f --estcrd-file= specify the final, estimated coordinates file"
+  echo "            This file is used to check if the stations were fixed or estimated"
   echo "           -h --help display (this) help message and exit"
   echo "           -v --version dsiplay version and exit"
   echo ""
@@ -101,12 +103,12 @@ getopt -T > /dev/null
 if [ $? -eq 4 ]
 then
   # GNU enhanced getopt is available
-  ARGS=`getopt -o y:d:g:c:s:x:vh \
-  -l year:,doy:,igs-file:,coordinate-file:,summary-file:,xs-diffs:,help,version \
+  ARGS=`getopt -o y:d:g:c:s:x:f:vh \
+  -l year:,doy:,igs-file:,coordinate-file:,summary-file:,xs-diffs:,estcrd-file:,help,version \
   -n 'marksolsta' -- "$@"`
 else
   # Original getopt is available (no long option names, no whitespace, no sorting)
-  ARGS=`getopt y:d:g:c:s:x:vh "$@"`
+  ARGS=`getopt y:d:g:c:s:x:f:vh "$@"`
 fi
 
 # check for getopt error
@@ -138,6 +140,9 @@ while true ; do
       shift;;
     -x|--xs-diffs)
       DIF_FILE="${2}"
+      shift;;
+    -f|--estcrd-file)
+      EST_FILE="${2}"
       shift;;
     -h|--help)
       help
@@ -189,9 +194,10 @@ then
     echo "ERROR. Must specify the diffs file"
     exit 1
 else
-    if ! test -f $DIF_FILE
+    if ! test -f "$DIF_FILE"
     then
-        echo "ERROR. Failed to diffs file: $DIF_FILE"
+        echo "if ! test -f $DIF_FILE"
+        echo "ERROR. Failed to locate diffs file: $DIF_FILE"
         exit 1
     fi
 fi
@@ -205,10 +211,31 @@ fi
 if test ${SUM_FILE:(-2)} == ".Z"
 then
     cp $SUM_FILE ${SUM_FILE##*/}
-    uncompress ${SUM_FILE##*/}
+    uncompress -f ${SUM_FILE##*/}
     SUM_FILE=${SUM_FILE##*/}
     SUM_FILE=${SUM_FILE/.Z/}
     RM_SUM_FILE=YES
+fi
+
+USE_EST_FILE=NO
+RM_EST_FILE=YES
+if ! test -z "$EST_FILE"
+then
+  if ! test -f "$EST_FILE"
+  then
+    echo "ERROR. Cannot locate estimated coordinates file $EST_FILE"
+    exit 1
+  else
+    echo "## Using file $EST_FILE for crd flags"
+    USE_EST_FILE=YES
+    if test ${EST_FILE:(-2)} == ".Z"
+    then
+      cp $EST_FILE ${EST_FILE##*/}
+      uncompress -f ${EST_FILE##*/}
+      EST_FILE=${EST_FILE##*/}
+      EST_FILE=${EST_FILE/.Z/}
+    fi
+  fi
 fi
 
 # //////////////////////////////////////////////////////////////////////////////
@@ -227,6 +254,11 @@ echo "##* **** ******* ******* ******* ****** ****** ****** ***  ********** ****
 ## Loop for 150 iterations or stop when an empty line is encountered
 ## Fir every non-empty line, print line to LOGFILE
 
+## we also need a new crd file containing all regional and isg coordinates
+cat $CRD_FILE > .tcrd
+cat $IGS_FILE >>.tcrd
+CRD_FILE=.tcrd
+
 # //////////////////////////////////////////////////////////////////////////////
 # WRITE TO OUTPUT THE PROCESSED STATIONS CRDs --- NEW AMIGUITY SUMMARY FILE
 # //////////////////////////////////////////////////////////////////////////////
@@ -236,15 +268,28 @@ STATUS=0
 
 ITERATOR=1
 ARRAY=$(awk '{ printf "%4s ",$1 }' $DIF_FILE)
-for i in ${ARRAY[*]}
+for i in ${ARRAY[@]}
 do
     if grep -i $i $CRD_FILE &>/dev/null
     then
         iter=$ITERATOR
         xyz=`grep -i $i $CRD_FILE | awk '{ print $3,$4,$5}'`
         flh=`echo $xyz | xyz2flh -ud | awk '{print $6,$7,$8}'`
-        diffs=`grep $i  $testxs| awk '{print $3,$4,$5,$6,$7,$8, "EST"}'`
+        diffs=`grep $i $DIF_FILE | awk '{print $3,$4,$5,$6,$7,$8, "EST"}'`
         iter=$(printf "%02d" $ITERATOR)
+        if test "$USE_EST_FILE" == "YES"
+        then
+            FLAG=`grep -i $i $EST_FILE | awk '{print substr ($0,69,3)}' | sed 's| ||g'`
+            #if test "$FLAG" == "A"; then FLAG=EST; fi
+            if test "$FLAG" == "W"
+            then
+              diffs=`echo $diffs | sed 's|EST|HLM|g'`
+            fi
+            if test "$FLAG" == "F"
+            then
+              diffs=`echo $diffs | sed 's|EST|FIX|g'`
+            fi
+        fi
         echo $iter $i $diffs $flh
         let ITERATOR=ITERATOR+1
     else
@@ -256,9 +301,51 @@ done
 # //////////////////////////////////////////////////////////////////////////////
 # WRITE TO OUTPUT THE PROCESSED BASELINES
 # //////////////////////////////////////////////////////////////////////////////
-NR_OF_BASELINES=`grep "NUMBER OF BASELINES :" $SUM_FILE | awk '{print $5}'`
-MEAN_BSL_LENGTH=`grep "MEAN BASELINE LENGTH:" $SUM_FILE | awk '{print $4}'`
-MEAN_AMB_RESOLVED=`grep "MEAN AMB. RESOLVED" $SUM_FILE | awk '{print $5}'`
+
+NR_OF_BASELINES=0
+MEAN_BSL_LENGTH=0
+MEAN_AMB_RESOLVED=0
+DEN_B=0
+DEN_P=0
+int='^[0-9]+$'
+flt='^[0-9]+([.][0-9]+)?$'
+for i in "#AR_NL" "#AR_L3" "#AR_QIF" "#AR_L12"
+do
+  MORE=`grep "Tot:" $SUM_FILE | grep ${i} | awk '{print $2}'`
+  if test "x$MORE" == "x"; then MORE=0; fi
+  if [[ $MORE =~ $int ]]
+  then
+    let NR_OF_BASELINES=NR_OF_BASELINES+$MORE
+  else
+    echo "ERROR. Cannot extract number of baselines!"
+    echo "Found string [$MORE] instead of integer"
+    exit 1
+  fi
+  LENGTH=`grep "Tot:" $SUM_FILE | grep ${i} | awk '{print $3}'`
+  if test "x$LENGTH" == "x"; then LENGTH=0; fi
+  if [[ $LENGTH =~ $flt ]]
+  then
+    NUM_B=$(awk -v num=$NUM_B -v l=$LENGTH -v m=$MORE 'BEGIN { print num + (l*m) }')
+    let DEN_B=DEN_B+$MORE
+  else
+    echo "ERROR. Cannot extract mean baseline length!"
+    echo "Found string [$LENGTH] instead of float"
+    exit 1
+  fi
+  PERC=`grep "Tot:" $SUM_FILE | grep ${i} | awk '{print $8}'`
+  if test "x$PERC" == "x"; then PERC=0; fi
+  if [[ $PERC =~ $flt ]]
+  then
+    NUM_P=$(awk -v num=$NUM_P -v p=$PERC -v m=$MORE 'BEGIN { print num + (p*m) }')
+    let DEN_P=DEN_P+$MORE
+  else
+    echo "ERROR. Cannot extract mean baseline percent!"
+    echo "Found string [$PERC] instead of float"
+    exit 1
+  fi
+done
+MEAN_BSL_LENGTH=$(echo "${NUM_B}/${DEN_B}" | bc)
+MEAN_AMB_RESOLVED=$(echo "${NUM_P}/${DEN_P}" | bc)
 echo "## NUMBER OF BASELINES : $NR_OF_BASELINES"
 echo "## MEAN BASELINE LENGTH: $MEAN_BSL_LENGTH"
 echo "## MEAN AMB. RESOLVED  : $MEAN_AMB_RESOLVED"
@@ -268,13 +355,7 @@ ITERATOR=0
 ## we are going to need 4 temp files
 >.tmp1
 >.tmp2
->.tcrd
 >.logf
-
-## we also need a new crd file containing all regional and isg coordinates
-cat $CRD_FILE > .tcrd
-cat $IGS_FILE >>.tcrd
-CRD_FILE=.tcrd
 
 ## First Code-Based Narrowlane
 
@@ -386,8 +467,45 @@ do
 done
 paste .tmp1 .tmp2 .logf | awk '{print "BL",$1,$2,$3,$4,$7,"L12"}'
 
+>.tmp1
+>.tmp2
+>.logf
+## Code-Based Narrowlane
+grep "#AR_NL" $SUM_FILE | head -n -1 | awk '{print $2,$3,$9}' > .logf
+ARRAY1=$(awk '{ printf "%4s ", $1 }' .logf)
+ARRAY2=$(awk '{ printf "%4s ", $2 }' .logf)
+for i in ${ARRAY1[*]}
+do
+    if grep -i $i $CRD_FILE &>/dev/null
+    then
+        xyz=`grep -i $i $CRD_FILE | awk '{print $3,$4,$5}'`
+        flh=`echo $xyz | xyz2flh -ud | awk '{print $6,$7}'`
+        #line1=`grep $i .logf`
+        echo $flh >> .tmp1
+    else
+        echo "## WARNING(1) : Station $i not found in crd file!"
+        STATUS=2
+    fi
+    let ITERATOR=ITERATOR+1
+done
+
+for i in ${ARRAY2[*]}
+do
+    if grep -i $i $CRD_FILE &>/dev/null
+    then
+        xyz=`grep -i $i $CRD_FILE | awk '{print $3,$4,$5}'`
+        flh=`echo $xyz | xyz2flh -ud | awk '{print $6,$7}'`
+        #line1=`grep $i .logf`
+        echo $flh >> .tmp2
+    else
+        echo "## WARNING(2) : Station $i not found in crd file!"
+        STATUS=2
+    fi
+done
+paste .tmp1 .tmp2 .logf | awk '{print "BL",$1,$2,$3,$4,$7,"CNL"}'
+
 ## Check the iterator to see how many baselines we translated
-if test $ITERATOR -ne $NR_OF_BASELINES
+if test "$ITERATOR" -ne "$NR_OF_BASELINES"
 then
   echo "## !! WARNING: $ITERATOR out of $NR_OF_BASELINES baselines translated!"
   STATUS=2
@@ -400,6 +518,13 @@ rm .logf .tmp1 .tmp2 .tcrd 2>/dev/null
 if test "$RM_SUM_FILE" == "YES"
 then
     rm $SUM_FILE
+fi
+if test "$USE_EST_FILE" == "YES"
+then
+  if test "$RM_EST_FILE" == "YES"
+  then
+    rm $EST_FILE
+  fi
 fi
 
 ## EXIT
