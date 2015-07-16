1fb0e9eef6448516c1a5df39377d3af5d2443c9d
xanthos@ntua <xanthos@mail.ntua.gr>
2015-07-16 15:51:44 +0300

    added gpstime.py

--- src/python/new/test/bamb.py
+++ src/python/new/test/bamb.py
@@ -1,43 +1,98 @@
 #! /usr/bin/python
 
 import os
+    
+''' Ambiguity resolution methods and their id-codes '''
+amb_method = {'Code-Based Widelane': '#AR_WL',
+    'Code-Based Narrowlane': '#AR_NL',
+    'Phase-Based Widelane': '#AR_L5',
+    'Phase-Based Narrowlane': '#AR_L3',
+    'Quasi-Ionosphere-Free': '#AR_QIF',
+    'Direct L1/L2': '#AR_L12'
+    }
+
+''' Ambiguity resolution method short names '''
+amb_keys   = {'cbwl': 'Code-Based Widelane',
+    'cbnl': 'Code-Based Narrowlane',
+    'pbwl': 'Phase-Based Widelane',
+    'pbnl': 'Phase-Based Narrowlane',
+    'qif': 'Quasi-Ionosphere-Free',
+    'l12': 'Direct L1/L2'
+    }
+
+''' Satellite System id-names '''
+satsys     = {'G': 'GPS', 'R': 'GLONASS', 'GR': 'MIXED'}
+
+''' Number of LC's for every method (i.e. columns of type: Max/RMS L5' '''
+amb_lcs   = {'cbwl': 1, 'cbnl': 1, 'pbwl': 1, 'pbnl': 1, 'qif': 2, 'l12': 1}
+
+def satsys2key(ss):
+    ss = ss.strip()
+    if ss == 'GPS' or ss == 'gps':
+        return 'G'
+    elif ss == 'GLONASS' or ss == 'glonass':
+        return 'R'
+    elif ss == 'MIXED' or ss == 'mixed':
+        return 'GR'
+    else:
+        raise RuntimeError('Invalid satellite system string: '+ss)
+
+def ambstr2key(ambs):
+    ambs = ambs.strip()
+    if ambs == '#AR_WL': return 'cbwl'
+    elif ambs == '#AR_NL' : return 'cbnl'
+    elif ambs == '#AR_L5' : return 'pbwl'
+    elif ambs == '#AR_L3' : return 'pbnl'
+    elif ambs == '#AR_QIF': return 'qif'
+    elif ambs == '#AR_L12': return 'l12'
+    else:
+        raise RuntimeError('Invalid ambiguity method string: '+ambs)
 
 class ambline:
-    __line = ''
+    __line      = ''
+    __lns       = []
+    __method    = ''
+    __amb_key   = ''
 
     def __init__(self,line):
         self.__line = line
+        self.__lns  = line.split()
+        try:
+            ambkey           = ambstr2key(self.__lns[len(self.__lns)-1]) ## e.g. 'qif'
+            self.__method    = amb_keys[ambkey] ## e.g. 'Quasi-Ionosphere-Free'
+            self.__amb_key   = ambkey
+            # self.__meth_name = amb_method[self.__method] ## e.g. '#AR_QIF'
+        except:
+            raise
+
+    def baseline(self):
+        return self.__lns[0][0:4]
+    def sta1(self):
+        return self.__lns[1]
+    def sta2(self):
+        return self.__lns[2]
+    def length(self):
+        return float(self.__lns[3])
+    def method(self):
+        return self.__amb_key
+    def ambsbefore(self):
+        return int(self.__lns[4]),float(self.__lns[5])
+    def ambsafter(self):
+        return int(self.__lns[6]),float(self.__lns[7])
+    def percent(self):
+        return float(self.__lns[8])
+    def satsys(self):
+        sys = self.__lns[9].strip()
+        try:
+            return satsys[sys]
+        except:
+            raise RuntimeError('Invalid satellite system string: '+sys)
+        
 
 class ambfile:
     ''' A class to hold a Bernese ambiguity summary file (.SUM)
     '''
     __filename   = '' #: The filename
-
-    ''' Ambiguity resolution methods and their id-codes '''
-    __amb_method = {'Code-Based Widelane': '#AR_WL',
-            'Code-Based Narrowlane': '#AR_NL',
-            'Phase-Based Widelane': '#AR_L5',
-            'Phase-Based Narrowlane': '#AR_L3',
-            'Quasi-Ionosphere-Free': '#AR_QIF',
-            'Direct L1/L2': '#AR_L12'
-            }
-
-    ''' Ambiguity resolution method short names '''
-    __amb_keys   = {'cbwl': 'Code-Based Widelane',
-            'cbnl': 'Code-Based Narrowlane',
-            'pbwl': 'Phase-Based Widelane',
-            'pbnl': 'Phase-Based Narrowlane',
-            'qif': 'Quasi-Ionosphere-Free',
-            'l12': 'Direct L1/L2'
-            }
-
-    ''' Satellite System id-names '''
-    __satsys     = {'GPS': 'G', 'GLONASS': 'R', 'MIXED': 'GR'}
-
-    ''' Number of LC's for every method (i.e. columns of type: Max/RMS L5' '''
-    __amb_lcs   = {'cbwl': 1, 'cbnl': 1, 'pbwl': 1, 'pbnl': 1, 'qif': 2,
-            'l12': 1}
-
 
     def __init__(self,filename):
         ''' Initialize a sta object given its filename;
@@ -46,6 +101,29 @@ class ambfile:
         if not os.path.isfile(filename):
             raise IOError('No such file '+filename)
         self.__filename = filename
+
+    def collectBsls(self,method,satsys='MIXED'):
+
+        ## satellite system key:
+        try:
+            sat_sys_key = satsys2key(satsys)
+        except:
+            raise
+
+        try:
+            bsls_list = self.collectMethodBsls(method)
+        except:
+            raise
+
+        final_list = []
+        for line in bsls_list:
+            j = ambline(line)
+            if j.method() != method:
+                raise RuntimeError('Something went wrong in collecting baselines! Methods dont match: '+ method + '-' + j.method())
+            if j.satsys() == satsys.upper():
+                final_list.append(line)
+
+        return final_list
 
     def collectMethodBsls(self,method):
 
@@ -65,10 +143,10 @@ class ambfile:
             fin.close()
             raise RuntimeError('Cannot collect baselines for resolution method '+method)
         elif line[0:3] == '===':
-            return
+            return []
 
-        smeth1 = self.__amb_keys[method]
-        smeth2 = self.__amb_method[smeth1]
+        smeth1 = amb_keys[method]
+        smeth2 = amb_method[smeth1]
 
         bsl_info = []
 
@@ -83,7 +161,6 @@ class ambfile:
             else:
                 bsl_info.append(line)
             line = fin.readline()
-            print line.strip()
 
         fin.close()
         return bsl_info
@@ -104,8 +181,8 @@ class ambfile:
         '''
 
         try:
-            smeth1 = self.__amb_keys[method]
-            smeth2 = self.__amb_method[smeth1]
+            smeth1 = amb_keys[method]
+            smeth2 = amb_method[smeth1]
         except:
             raise RuntimeError('Invalid resolution method '+method)
 
@@ -149,3 +226,8 @@ print x.collectMethodBsls('l12')
 print x.collectMethodBsls('cbnl')
 print x.collectMethodBsls('pbwl')
 print x.collectMethodBsls('qif')
+
+print x.collectBsls('l12','gps')
+print x.collectBsls('cbnl','glonass')
+print x.collectBsls('pbwl','gps')
+print x.collectBsls('qif')
